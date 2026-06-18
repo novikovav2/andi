9062a3fa1439b8e7485c3ac635eb121a93c2b049
Aleksandr Novikov <novikovav2@gmail.com>
2026-06-19 06:14:58 +1000

    Update 500 error page

--- test/controllers/rails_internal_routes_test.rb
+++ test/controllers/rails_internal_routes_test.rb
@@ -32,6 +32,19 @@ class RailsInternalRoutesTest < ActionDispatch::IntegrationTest
     assert_includes response.body, "Такой страницы нет"
   end
 
+  test "static server error page matches application error style" do
+    body = Rails.root.join("public/500.html").read
+
+    assert_includes body, "<main class=\"card\">"
+    assert_includes body, "src=\"/icons/android-192.png\""
+    assert_includes body, "😵 Что-то пошло не так"
+    assert_includes body, "Во время обработки запроса произошла ошибка."
+    assert_includes body, "Попробуйте обновить страницу через несколько минут."
+    assert_includes body, "Если проблема повторяется, вернитесь на главную и попробуйте снова."
+    assert_includes body, "<a href=\"/\">На главную</a>"
+    assert_no_match(/stack trace|backtrace|exception|error id/i, body)
+  end
+
   private
 
   def assert_not_custom_not_found
